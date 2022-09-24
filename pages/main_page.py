2e16e6d23d4ba59d6c0ea39851b00b11f998a1a6
Daniil Tkalich <103593586+tdanielv@users.noreply.github.com>
2022-09-24 15:50:39 +0400

    Had deal with assert"s errors

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,3 +6,6 @@ class Main_Page(Base_Page):
     def got_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, '#login_link')
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, '#login_link_invalid'), 'Login link is not presented'
