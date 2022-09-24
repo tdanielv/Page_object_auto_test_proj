c5ef2ae22b25d9c37cf26dbb05f8873174b09fed
Daniil Tkalich <103593586+tdanielv@users.noreply.github.com>
2022-09-24 17:57:43 +0400

    1st method of choosing between sites

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,8 @@ login_link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
 def test_guest_can_go_to_login_page(browser):
     page = Main_Page(browser, link_main_page)
     page.open()
-    page.got_to_login_page()
+    login_page = page.got_to_login_page()
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     page = Main_Page(browser, link_main_page)
